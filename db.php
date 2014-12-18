853a5fb9ea32cab1d343b5cea413d68ccfd07eb8
Ria-Fox <jspak3@naver.com>
2014-12-18 17:03:22 +0900

    Revert "first source"
    This reverts commit e97eb2659550986694b42c1fbbb8b118f0ca9a70.

--- db.php
+++
@@ -1,7 +0,0 @@
-<?php
-function connect(){
-	$conn = mysql_connect("brynn.postech.ac.kr","rienkim", "20110437") or die("Unable to select database");
-	mysql_select_db("test", $conn) or die("Unable to select database");
-	return $conn;
-}
-?>
