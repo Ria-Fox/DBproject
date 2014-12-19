066362c2ff6bf56655a26b0cc1680620d1d93aa7
Ria-Fox <jspak3@naver.com>
2014-12-20 04:06:41 +0900

    join page

---
+++ join.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ko">
+   <head>
+		<meta charset="utf-8" http-equiv="ContentType" content="text/html;charset=UTF-8"/>
+			<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0"/>
+				<link href="css/bootstrap.min.css" rel="stylesheet" media="screen"/>
+	 </head>
+	<body style="padding:5px">
+		<script src="//code.jquery.com/jquery.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/respond.js"></script>
+		<table border=0>
+		   <tr>
+			  <td><center><h1>PosHotel <small>join page</small></h1></center></td>
+		   </tr>
+		   <tr>
+			  <td><a href="userjoin.php">HOTEL USER</a></td>
+			  <td><a href="ownerjoin.php">HOTEL OWNER</a></td>
+		   </tr>
+		</table>
+	 </body>
+  </html>
+
