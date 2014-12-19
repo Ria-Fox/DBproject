fdd80709b5daaf43acc39f70f3221c21faa5b344
Ria-Fox <jspak3@naver.com>
2014-12-19 17:54:48 +0900

    login

---
+++ login.php
@@ -0,0 +1,53 @@
+<html>
+   <head><title></title></head>
+   <body>
+	  <script language="javascript" type="text/javascript">
+
+<?php
+   include("db.php");
+   $conn=connect();
+   session_start();
+   $id=$_REQUEST['user_id'];
+   $pass=$_REQUEST['user_pw'];
+   $owner=$_REQUEST['isowner'];
+   if(!$id || !$pass){
+	  echo("<script>
+		 window.alert('Please input your id and password')
+		 history.go(-1)
+	  </script>");
+   }
+   if(!$owner){
+	  $query="select `uid` from `user` where `uid`=`$id` and `pw`=`$pass`";
+	  $result=mysql_query($query);
+	  $count=mysql_num_rows($result);
+	  if($count==1){
+		 $_SESSION["type"]="user";
+		 $_SESSION["id"]=$id;
+		 header("location:index.php");
+	  }
+	  else{
+		 echo("<script>
+			window.alert('Login Failed')
+			history.go(-1)
+		 </script>");
+	  }
+   }
+   else{
+	  $query="select `uid` from `owner` where `uid`=`$id` and `pw`=`$pass`";
+	  $result=mysql_query($query);
+	  $count=mysql_num_rows($result);
+	  if($count==1){
+		 $_SESSION["type"]="owner";
+		 $_SESSION["id"]=$id;
+		 header("location:owner.php");
+	  }
+	  else{
+		 echo("<script>
+			window.alert('Login Failed')
+			history.go(-1)
+		 </script>");
+	  }
+   }
+?>
+</body>
+</html>
