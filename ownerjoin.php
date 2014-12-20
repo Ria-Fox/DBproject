52e32260107261a1d7bc879a11333e7ee69e9415
ymfact <123young321@naver.com>
2014-12-20 13:22:52 +0900

    가입 페이지 개선

--- ownerjoin.php
+++ ownerjoin.php
@@ -1,38 +1,37 @@
-<?php
-   include("db.php");
-   $conn=connect();
-   session_start();
-   if($_SERVER["REQUEST_METHOD"] == "POST"){
-     $id=$_REQUEST['id'];
-    $pw=$_REQUEST['pw'];
-	$phone=$_REQUEST['phone'];
-	  $query="select count(*) from `owner` where `oid`= ".$id;
-	  $row=mysql_query($query,$conn);
-	  $rs=mysql_fetch_array($row);
-	  $isid=$rs[0];
-    if((!$id || !$pw || !$phone ) && $_REQUEST['submitted']){
-	  echo("<script>
-		 window.alert('Please input all fields.')
-		 history.go(-1)
-	  </script>");
-   }
-   elseif($isid>0){
-	  echo("<script>
-		 window.alert('Registered ID')
-		 history.go(-1)
-	  </script>");
-   }
-   else{
-	  $query="insert `owner` values (".$id.", ".$pw.", ".$phone.")";
-	$row=mysql_query($query, $conn);
-	$_SESSION['type']="owner";
-	$_SESSION['id']=$id;
-	header("location:owner.php");
-
- }
- }
- if(!$_REQUEST['id']){
-	header("location:join.php");
- }
-
-?>
+<!DOCTYPE html>
+<html lang="ko">
+   <head>
+		<meta charset="utf-8" http-equiv="ContentType" content="text/html;charset=UTF-8"/>
+				<link href="css/bootstrap.min.css" rel="stylesheet" media="screen"/>
+	 </head>
+	<body style="padding:5px">
+		<script src="//code.jquery.com/jquery.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<form action="ownerjoinproc.php" method="post">
+		<center><div class="panel panel-default" style="width:640px">
+		<div class="panel-heading">
+		<center><h1>PosHotel <small>join page</small></h1></center>
+		</div>
+  		<div class="panel-body">
+		<table class="table">
+		   <tr>
+			  <td align="right">ID : </td>
+			  <td><input type="text" name="id"></td>
+		   </tr>
+		   <tr>
+			  <td align="right">Password : </td>
+			  <td><input type="password" name="pw"></td>
+		   </tr>
+		   <tr>
+			  <td align="right">Phone Number : </td>
+			  <td><input type="text" name="phone"></td>
+		   </tr>
+		</table>
+		</div>
+		<div class="panel-footer" align="right">
+			<button type="button" class="btn btn-default" onclick="history.go(-1)">취소</button>
+			<input type="hidden" name="submitted" value=1><input type="submit" class="btn btn-primary" value="가입하기">
+		</div></center>
+		</form>
+	 </body>
+  </html>
