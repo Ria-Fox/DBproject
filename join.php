5aeac5c02d06747b6ca80f07e32cb942296ae8b4
ymfact <123young321@naver.com>
2014-12-20 13:23:04 +0900

    Revert "가입 페이지 개선"
    This reverts commit 52e32260107261a1d7bc879a11333e7ee69e9415.

---
+++ join.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ko">
+   <head>
+		<meta charset="utf-8" http-equiv="ContentType" content="text/html;charset=UTF-8"/>
+				<link href="css/bootstrap.min.css" rel="stylesheet" media="screen"/>
+	 </head>
+	<body style="padding:5px">
+		<script src="//code.jquery.com/jquery.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<table border=0>
+		   <tr>
+			  <td><center><h1>PosHotel <small>join page</small></h1></center></td>
+		   </tr>
+		   <tr>
+			  <td><a href="userjoin.html">HOTEL USER</a></td>
+			  <td><a href="ownerjoin.html">HOTEL OWNER</a></td>
+		   </tr>
+		</table>
+	 </body>
+  </html>
+
