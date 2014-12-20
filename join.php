f042fe121b601cf94db398e247bf3d5965b604f5
ymfact <123young321@naver.com>
2014-12-20 12:56:05 +0900

    반응성 일괄삭제

--- join.php
+++ join.php
@@ -2,13 +2,11 @@
 <html lang="ko">
    <head>
 		<meta charset="utf-8" http-equiv="ContentType" content="text/html;charset=UTF-8"/>
-			<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0"/>
 				<link href="css/bootstrap.min.css" rel="stylesheet" media="screen"/>
 	 </head>
 	<body style="padding:5px">
 		<script src="//code.jquery.com/jquery.js"></script>
 		<script src="js/bootstrap.min.js"></script>
-		<script src="js/respond.js"></script>
 		<table border=0>
 		   <tr>
 			  <td><center><h1>PosHotel <small>join page</small></h1></center></td>
